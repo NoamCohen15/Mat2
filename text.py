b9ad3319001fa7925ce1c9871d5d7c6116e0e067
NoamCohen15 <noamcohen1503@gmail.com>
2021-04-01 22:02:18 +0300

    new

---
+++ text.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+  
+def countword()->int:
+    file= open("text.txt", "r")
+    seq= file.read()
+    print(seq)
+    text= seq.split ()
+    word= text[0]
+    count=1
+    for cur in text[1:]:
+        txt= revword(cur)
+        if word==txt:
+            count+=1
+    return count
+      
+def revword(word:str) -> str:
+    word = word.lower()
+    return word[::-1]
+
